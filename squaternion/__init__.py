582ff5e7edc650e5505d78a530d25e4e010bfc63
walchko <walchko@users.noreply.github.com>
2021-11-20 18:49:32 -0700

    upgrade python

--- squaternion/__init__.py
+++ squaternion/__init__.py
@@ -7,10 +7,10 @@
 from squaternion.squaternion import Quaternion
 from squaternion.squaternion import Axis
 
-try:
-    from importlib.metadata import version # type: ignore
-except ImportError:
-    from importlib_metadata import version # type: ignore
+# try:
+from importlib.metadata import version # type: ignore
+# except ImportError:
+#     from importlib_metadata import version # type: ignore
 
 __author__ = "Kevin Walchko"
 __license__ = "MIT"
